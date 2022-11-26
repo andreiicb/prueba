9c15bd0cee41f6986f44c4bb51a27712762124c8
andrei <andreicolio58@gmailcom>
2022-11-25 17:09:07 -0700

    Agregamos ciclo de contar del 1 al 10

--- prueba.java
+++ prueba.java
@@ -2,5 +2,10 @@ class prueba{
     public static void main(String[] args) {
         
         System.out.println("Hola mundo!");
+
+        for(int i=0;i<10;i++){
+            System.out.println("Numero: "+i);
+        }
+
     }
 }
